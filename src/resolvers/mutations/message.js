b4a03303e0b57f5fc505323ada7993ff5fc22769
Gh05d <pascal.clanget@googlemail.com>
2018-01-15 18:38:49 +0100

    Protected routes

--- src/resolvers/mutations/message.js
+++ src/resolvers/mutations/message.js
@@ -56,55 +56,57 @@ export default {
     }
   ),
 
-  sendMessage: async (parent, { fromuser, touser, message }, { models }) => {
-    const sender = await models.User.findById(fromuser);
-    const receiver = await models.User.findById(touser);
+  sendMessage: requiresAuth.createResolver(
+    async (parent, { fromuser, touser, message }, { models }) => {
+      const sender = await models.User.findById(fromuser);
+      const receiver = await models.User.findById(touser);
 
-    if (!sender || !receiver) {
-      return {
-        ok: false,
-        error: "User doesn't exist!"
-      };
-    } else if (sender.id == receiver.id) {
-      return {
-        ok: false,
-        error: "Sender and Receiver can't be the same User!"
-      };
-    } else if (message && sender && receiver) {
-      try {
-        const save = await models.Notification.create({
-          fromuser,
-          touser,
-          type: 1,
-          message
-        });
-
-        const newMessage = {
-          ...save.dataValues,
-          fromuser: sender.dataValues,
-          touser: receiver.dataValues
-        };
-        console.log(newMessage);
-        pubsub.publish(NEW_MESSAGE, {
-          userId: receiver.id,
-          newMessage
-        });
-
-        return {
-          ok: true,
-          message
-        };
-      } catch (err) {
+      if (!sender || !receiver) {
         return {
           ok: false,
-          error: err.message
+          error: "User doesn't exist!"
+        };
+      } else if (sender.id == receiver.id) {
+        return {
+          ok: false,
+          error: "Sender and Receiver can't be the same User!"
+        };
+      } else if (message && sender && receiver) {
+        try {
+          const save = await models.Notification.create({
+            fromuser,
+            touser,
+            type: 1,
+            message
+          });
+
+          const newMessage = {
+            ...save.dataValues,
+            fromuser: sender.dataValues,
+            touser: receiver.dataValues
+          };
+          console.log(newMessage);
+          pubsub.publish(NEW_MESSAGE, {
+            userId: receiver.id,
+            newMessage
+          });
+
+          return {
+            ok: true,
+            message
+          };
+        } catch (err) {
+          return {
+            ok: false,
+            error: err.message
+          };
+        }
+      } else {
+        return {
+          ok: false,
+          error: "Empty Message!"
         };
       }
-    } else {
-      return {
-        ok: false,
-        error: "Empty Message!"
-      };
     }
-  }
+  )
 };
--- src/resolvers/queries/message.js
+++ src/resolvers/queries/message.js
@@ -3,62 +3,64 @@ import { Op } from "sequelize";
 import _ from "lodash";
 
 export default {
-  fetchMessages: async (parent, { id, read }, { models }) => {
-    let users;
-    let apps;
+  fetchMessages: requiresAuth.createResolver(
+    async (parent, { id, read }, { models }) => {
+      let users;
+      let apps;
 
-    if (read === true) {
-      users = await models.Notification.findAll({
-        where: {
-          touser: id,
-          deleted: false,
-          readtime: { [Op.not]: null }
-        },
-        order: [["sendtime", "DESC"]]
-      });
+      if (read === true) {
+        users = await models.Notification.findAll({
+          where: {
+            touser: id,
+            deleted: false,
+            readtime: { [Op.not]: null }
+          },
+          order: [["sendtime", "DESC"]]
+        });
 
-      apps = await models.AppNotification.findAll({
-        where: {
-          touser: id,
-          deleted: false,
-          readtime: { [Op.not]: null }
-        },
-        order: [["sendtime", "DESC"]]
-      });
-    } else if (read === false) {
-      users = await models.Notification.findAll({
-        where: {
-          touser: id,
-          deleted: false,
-          readtime: { [Op.eq]: null }
-        },
-        order: [["sendtime", "DESC"]]
-      });
+        apps = await models.AppNotification.findAll({
+          where: {
+            touser: id,
+            deleted: false,
+            readtime: { [Op.not]: null }
+          },
+          order: [["sendtime", "DESC"]]
+        });
+      } else if (read === false) {
+        users = await models.Notification.findAll({
+          where: {
+            touser: id,
+            deleted: false,
+            readtime: { [Op.eq]: null }
+          },
+          order: [["sendtime", "DESC"]]
+        });
 
-      apps = await models.AppNotification.findAll({
-        where: {
-          touser: id,
-          deleted: false,
-          readtime: { [Op.eq]: null }
-        },
-        order: [["sendtime", "DESC"]]
-      });
-    } else {
-      users = await models.Notification.findAll({
-        where: { touser: id, deleted: false },
-        order: [["sendtime", "DESC"]]
-      });
+        apps = await models.AppNotification.findAll({
+          where: {
+            touser: id,
+            deleted: false,
+            readtime: { [Op.eq]: null }
+          },
+          order: [["sendtime", "DESC"]]
+        });
+      } else {
+        users = await models.Notification.findAll({
+          where: { touser: id, deleted: false },
+          order: [["sendtime", "DESC"]]
+        });
 
-      apps = await models.AppNotification.findAll({
-        where: { touser: id, deleted: false },
-        order: [["sendtime", "DESC"]]
-      });
+        apps = await models.AppNotification.findAll({
+          where: { touser: id, deleted: false },
+          order: [["sendtime", "DESC"]]
+        });
+      }
+
+      const result = [];
+      users.map(user => result.push(user));
+      apps.map(app => result.push(app));
+
+      return _.orderBy(result, "sendtime", "desc");
     }
-
-    const result = [];
-    users.map(user => result.push(user));
-    apps.map(app => result.push(app));
-
-    return _.orderBy(result, "sendtime", "desc");
-  }
+  )
 };
