f28e307ef6710e4b6fae220f1b3f3496bf7afb29
Jannis Froese <jf@vipfy.com>
2018-02-07 17:24:58 +0100

    make prepare_vipfy_test_db.sh respect .env file

--- prepare_vipfy_test_db.sh
+++ prepare_vipfy_test_db.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
+#load .env.test. This has to contain all settings since I ommitted setting default values above
+export $(cat .env.test | grep -v ^# | xargs)
+
 echo dropping database
-PGPASSWORD=testDatabase dropdb -U vipfy_development -h localhost -p 5433 vipfy_test_database
+PGPASSWORD=$DB_PW dropdb -U $DB_USER -h $DB_IP -p $DB_PORT $DB_NAME
 echo creating database
-PGPASSWORD=testDatabase createdb -U vipfy_development -h localhost -p 5433 vipfy_test_database
+PGPASSWORD=$DB_PW createdb -U $DB_USER -h $DB_IP -p $DB_PORT $DB_NAME
 
 echo restoring
-date
-time psql vipfy_test_database < vipfy_test_dump.sql
-date
+PGPASSWORD=$DB_PW time psql -U $DB_USER -h $DB_IP -p $DB_PORT $DB_NAME < vipfy_test_dump.sql
 echo finished restoring
