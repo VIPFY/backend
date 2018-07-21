6caf298e0cbe426679aad7e46a9885e41b9306c7
Jannis Froese <jf@vipfy.com>
2018-07-21 22:12:47 +0200

    other array resolver

--- src/resolvers/CustomResolvers.js
+++ src/resolvers/CustomResolvers.js
@@ -70,7 +70,7 @@ export const find = data => {
           if (data[search][0] == "[") {
             // return array of objects
             data[search] = data[search].substring(1, data[search].length - 1);
-            return models[data[search]].findAll({ where: { id: { $in: parent[search] } } });
+            return parent[search].map(s => models[data[search]].findById(s));
           } else {
             // single object
             return models[data[search]].findById(parent[search]);
