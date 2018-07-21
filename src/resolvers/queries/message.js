1f024d95b55f3a52f32c771a74d6a249d102a743
Jannis Froese <jf@vipfy.com>
2018-07-21 19:07:36 +0200

    remove old code

--- src/resolvers/queries/message.js
+++ src/resolvers/queries/message.js
@@ -3,43 +3,6 @@ import { decode } from "jsonwebtoken";
 import { requiresAuth } from "../../helpers/permissions";
 
 export default {
-  fetchMessages: requiresAuth.createResolver(async (parent, { read }, { models, token }) => {
-    let messages;
-    const {
-      user: { unitid }
-    } = decode(token);
-
-    try {
-      if (read === true) {
-        messages = await models.Message.findAll({
-          where: {
-            receiver: unitid,
-            archivetimereceiver: { [Op.eq]: null },
-            readtime: { [Op.not]: null }
-          },
-          order: [["sendtime", "DESC"]]
-        });
-      } else if (read === false) {
-        messages = await models.Message.findAll({
-          where: {
-            receiver: unitid,
-            archivetimereceiver: { [Op.eq]: null },
-            readtime: { [Op.eq]: null }
-          },
-          order: [["sendtime", "DESC"]]
-        });
-      } else {
-        messages = await models.Message.findAll({
-          where: { receiver: unitid, archivetimereceiver: { [Op.eq]: null } },
-          order: [["sendtime", "DESC"]]
-        });
-      }
-
-      return messages;
-    } catch (err) {
-      throw new Error(err.message);
-    }
-  }),
 
   fetchLastDialogMessages: requiresAuth.createResolver(async (parent, args, { models, token }) => {
     try {
