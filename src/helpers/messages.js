c393296c03b39d4d0b362af2b45e909ef1046cbd
Jannis Froese <jf@vipfy.com>
2018-07-30 12:19:49 +0200

    I broke the build :(

--- src/helpers/messages.js
+++ src/helpers/messages.js
@@ -1,15 +1,14 @@
 import { decode } from "jsonwebtoken";
 import sanitizeHtml from "sanitize-html";
 
-export const sanitizeMessage = html => sanitizeHtml(message, { allowProtocolRelative: false, allowedTags: ["em", "strong", "b", "i", "br", "p"]});
+export const sanitizeMessage = html => sanitizeHtml(html, { allowProtocolRelative: false, allowedTags: ["em", "strong", "b", "i", "br", "p"]});
 
 export const updateLastReadMessage = async (models, unit, group, messageid) => {
-    await models.MessageGroupMembership.update({ lastreadmessageid: messageid }, { where: { unitid: unit, group: group, visibletimeend } });
-    models.sequelize
-        .query(
-          `UPDATE messagegroupmembership_date
-          SET lastreadmessageid = :messageid
-          WHERE unitid = :unit AND groupid = :group
-          AND visibletimeend <= now()`,
-          { replacements: { unit, group, messageid } }
+  await models.sequelize
+    .query(
+      `UPDATE messagegroupmembership_data
+      SET lastreadmessageid = :messageid
+      WHERE unitid = :unit AND groupid = :group
+      AND visibletimeend <= now()`,
+      { replacements: { unit, group, messageid } });
 }
