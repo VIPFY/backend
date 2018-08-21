a3e1c0c2a721e8f27a7b94473a167b39980902d9
Jannis Froese <jf@vipfy.com>
2018-08-21 19:03:03 +0200

    Revert "forgot to safe a file ..."
    This reverts commit 35cc0706957e23e692dfe59360c529ef9fd9961c.

--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 import { random } from "lodash";
 import moment from "moment";
-import models from "vipfy-sequelize-setup";
+import models from "../models";
 
 /* eslint-disable no-return-assign */
 
