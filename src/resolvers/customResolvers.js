e84082872643017bb1813638784c70e23f6daf5c
Jannis Froese <jf@vipfy.com>
2018-08-22 14:48:06 +0200

    more error handling in find

--- src/resolvers/customResolvers.js
+++ src/resolvers/customResolvers.js
@@ -60,52 +60,62 @@ export const find = data => {
   const searches = {};
   Object.keys(data).map(search => {
     searches[search] = (parent, args, { models }, info) => {
-      switch (data[search]) {
-        case "Human":
-        case "Department":
-          return models[data[search]].findOne({
-            where: { unitid: parent.dataValues[search] }
-          });
-
-        default: {
-          if (data[search][0] == "[") {
-            // return array of objects
-            const modelName = data[search].substring(1, data[search].length - 1);
-            return models[modelName].findAll({
-              where: { id: { $in: parent[search] } }
+      try {
+        switch (data[search]) {
+          case "Human":
+          case "Department":
+            return models[data[search]].findOne({
+              where: { unitid: parent.dataValues[search] }
             });
-          } else {
-            // single object
-            /* console.error(
-              "FIND",
-              search,
-              data[search],
-              parent[search],
-              "INFO",
-              info,
-              "SET",
-              info.fieldNodes[0].selectionSet,
-              "S",
-              info.fieldNodes[0].selectionSet.selections
-            ); */
-            // models[data[search]].findById(parent[search], { raw: true })
-            // .then(a => console.error(parent[search], a));
-            if (
-              info.fieldNodes[0].selectionSet.selections.filter(
-                selection =>
-                  !selection.name ||
-                  (selection.name.value != "id" && selection.name.value != "__typename")
-              ).length == 0
-            ) {
-              if (parent[search] === null) {
-                return null;
-              } else {
-                return { id: parent[search] };
+
+          default: {
+            if (data[search][0] == "[") {
+              // return array of objects
+              const modelName = data[search].substring(
+                1,
+                data[search].length - 1
+              );
+              return models[modelName].findAll({
+                where: { id: { $in: parent[search] } }
+              });
+            } else {
+              // single object
+              /* console.error(
+                "FIND",
+                search,
+                data[search],
+                parent[search],
+                "INFO",
+                info,
+                "SET",
+                info.fieldNodes[0].selectionSet,
+                "S",
+                info.fieldNodes[0].selectionSet.selections
+              ); */
+              // models[data[search]].findById(parent[search], { raw: true })
+              // .then(a => console.error(parent[search], a));
+              if (
+                info.fieldNodes[0].selectionSet.selections.filter(
+                  selection =>
+                    !selection.name ||
+                    (selection.name.value != "id" &&
+                      selection.name.value != "__typename")
+                ).length == 0
+              ) {
+                if (parent[search] === null) {
+                  return null;
+                } else {
+                  return { id: parent[search] };
+                }
               }
+              return models[data[search]].findById(parent[search], {
+                raw: true
+              });
             }
-            return models[data[search]].findById(parent[search], { raw: true });
           }
         }
+      } catch (err) {
+        throw new NormalError({ message: "there was an error in the universal resolver" });
       }
     };
   });
