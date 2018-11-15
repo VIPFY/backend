dbb5c20c99a87dc4e059142ceb5db98266815fee
Jannis Froese <jf@vipfy.com>
2018-11-15 17:16:17 +0100

    bump version to 0.5.0

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,88 +3,91 @@ module.exports = {
    * Application configuration section
    * http://pm2.keymetrics.io/docs/usage/application-declaration/
    */
-  apps : [
+  apps: [
     {
-      name      : 'vipfy-backend',
-      script    : 'dist/index.js',
-      max_memory_restart: '1024M',
+      name: "vipfy-backend",
+      script: "dist/index.js",
+      max_memory_restart: "1024M",
       env: {
-        NODE_ENV: 'production',
-        ENVIRONMENT: 'production',
-        SSL_KEY: '/etc/letsencrypt/live/dev.vipfy.com/privkey.pem',
-        SSL_CERT: '/etc/letsencrypt/live/dev.vipfy.com/cert.pem',
+        NODE_ENV: "production",
+        ENVIRONMENT: "production",
+        SSL_KEY: "/etc/letsencrypt/live/dev.vipfy.com/privkey.pem",
+        SSL_CERT: "/etc/letsencrypt/live/dev.vipfy.com/cert.pem",
         GCLOUD_TRACE_NEW_CONTEXT: 1,
         USE_VOYAGER: 1
       },
-      env_production : {
-        NODE_ENV: 'production',
-        ENVIRONMENT: 'production',
-        SSL_KEY: '/etc/letsencrypt/live/vipfy.com/privkey.pem',
-        SSL_CERT: '/etc/letsencrypt/live/vipfy.com/cert.pem',
+      env_production: {
+        NODE_ENV: "production",
+        ENVIRONMENT: "production",
+        SSL_KEY: "/etc/letsencrypt/live/vipfy.com/privkey.pem",
+        SSL_CERT: "/etc/letsencrypt/live/vipfy.com/cert.pem",
         GCLOUD_TRACE_NEW_CONTEXT: 1
       },
       env_conf: {
-        NODE_ENV: 'production',
-        ENVIRONMENT: 'production',
-        SSL_KEY: '/etc/letsencrypt/live/conferences.vipfy.store/privkey.pem',
-        SSL_CERT: '/etc/letsencrypt/live/conferences.vipfy.store/cert.pem',
+        NODE_ENV: "production",
+        ENVIRONMENT: "production",
+        SSL_KEY: "/etc/letsencrypt/live/conferences.vipfy.store/privkey.pem",
+        SSL_CERT: "/etc/letsencrypt/live/conferences.vipfy.store/cert.pem",
         GCLOUD_TRACE_NEW_CONTEXT: 1,
         USE_VOYAGER: 1
       }
     }
   ],
- 
+
   /**
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy : {
-    production : {
-      user : 'node',
-      host : '35.205.22.68',
-      ref  : 'origin/master',
-      repo : 'git@bitbucket.org:vipfymarketplace/vipfy-backend.git',
-      path : '/var/www/vipfy-backend',
+  deploy: {
+    production: {
+      user: "node",
+      host: "35.205.22.68",
+      ref: "origin/release/0.5",
+      repo: "git@bitbucket.org:vipfymarketplace/vipfy-backend.git",
+      path: "/var/www/vipfy-backend",
       env: {
-        NODE_ENV: 'production',
-        ENVIRONMENT: 'production',
-        SSL_KEY: '/etc/letsencrypt/live/vipfy.com/privkey.pem',
-        SSL_CERT: '/etc/letsencrypt/live/vipfy.com/cert.pem',
+        NODE_ENV: "production",
+        ENVIRONMENT: "production",
+        SSL_KEY: "/etc/letsencrypt/live/vipfy.com/privkey.pem",
+        SSL_CERT: "/etc/letsencrypt/live/vipfy.com/cert.pem",
         GCLOUD_TRACE_NEW_CONTEXT: 1
       },
-      'post-deploy' : 'rm -rf node_modules/\@vipfy-private/; npm install && rm -rf dist && node_modules/.bin/babel src -d dist --ignore tests --copy-files && sh insert_profiling.sh && pm2 startOrRestart ecosystem.config.js --env production'
+      "post-deploy":
+        "rm -rf node_modules/@vipfy-private/; npm install && rm -rf dist && node_modules/.bin/babel src -d dist --ignore tests --copy-files && sh insert_profiling.sh && pm2 startOrRestart ecosystem.config.js --env production"
     },
-    dev : {
-      user : 'node',
-      host : '35.197.80.2',
-      ref  : 'origin/development',
-      repo : 'git@bitbucket.org:vipfymarketplace/vipfy-backend.git',
-      path : '/var/www/vipfy-backend',
+    dev: {
+      user: "node",
+      host: "35.197.80.2",
+      ref: "origin/development",
+      repo: "git@bitbucket.org:vipfymarketplace/vipfy-backend.git",
+      path: "/var/www/vipfy-backend",
       env: {
-        NODE_ENV: 'production',
-        ENVIRONMENT: 'production',
-        SSL_KEY: '/etc/letsencrypt/live/dev.vipfy.com/privkey.pem',
-        SSL_CERT: '/etc/letsencrypt/live/dev.vipfy.com/cert.pem',
+        NODE_ENV: "production",
+        ENVIRONMENT: "production",
+        SSL_KEY: "/etc/letsencrypt/live/dev.vipfy.com/privkey.pem",
+        SSL_CERT: "/etc/letsencrypt/live/dev.vipfy.com/cert.pem",
         GCLOUD_TRACE_NEW_CONTEXT: 1,
         USE_VOYAGER: 1
       },
-      'post-deploy' : 'rm -rf node_modules/\@vipfy-private/; npm install && rm -rf dist && node_modules/.bin/babel src -d dist --ignore tests --copy-files && sh insert_profiling.sh && pm2 startOrRestart ecosystem.config.js'
+      "post-deploy":
+        "rm -rf node_modules/@vipfy-private/; npm install && rm -rf dist && node_modules/.bin/babel src -d dist --ignore tests --copy-files && sh insert_profiling.sh && pm2 startOrRestart ecosystem.config.js"
     },
-    conf : {
-      user : 'node',
-      host : '104.155.3.91',
-      ref  : 'origin/development',
-      repo : 'git@bitbucket.org:vipfymarketplace/vipfy-backend.git',
-      path : '/var/www/vipfy-backend',
+    conf: {
+      user: "node",
+      host: "104.155.3.91",
+      ref: "origin/development",
+      repo: "git@bitbucket.org:vipfymarketplace/vipfy-backend.git",
+      path: "/var/www/vipfy-backend",
       env: {
-        NODE_ENV: 'production',
-        ENVIRONMENT: 'production',
-        SSL_KEY: '/etc/letsencrypt/live/conferences.vipfy.store/privkey.pem',
-        SSL_CERT: '/etc/letsencrypt/live/conferences.vipfy.store/cert.pem',
+        NODE_ENV: "production",
+        ENVIRONMENT: "production",
+        SSL_KEY: "/etc/letsencrypt/live/conferences.vipfy.store/privkey.pem",
+        SSL_CERT: "/etc/letsencrypt/live/conferences.vipfy.store/cert.pem",
         GCLOUD_TRACE_NEW_CONTEXT: 1,
         USE_VOYAGER: 1
       },
-      'post-deploy' : 'rm -rf node_modules/\@vipfy-private/; npm install && rm -rf dist && node_modules/.bin/babel src -d dist --ignore tests --copy-files && sh insert_profiling.sh && pm2 startOrRestart ecosystem.config.js --env conf'
+      "post-deploy":
+        "rm -rf node_modules/@vipfy-private/; npm install && rm -rf dist && node_modules/.bin/babel src -d dist --ignore tests --copy-files && sh insert_profiling.sh && pm2 startOrRestart ecosystem.config.js --env conf"
     }
   }
 };
