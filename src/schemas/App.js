f2f2732568a61285a5cff2e9aecd430777e545c8
gh05d <pc@vipfy.com>
2018-11-26 11:59:00 +0100

    changes

--- src/schemas/App.js
+++ src/schemas/App.js
@@ -96,7 +96,6 @@ export const types = `
 export const queries = `
   # Returns all apps in Vipfy
   allApps(limit: Int, offset: Int, sortOptions: SortOptions): [AppDetails]!
-
   fetchAllAppsEnhanced: [App]!
 
   # Returns a specific app by id
