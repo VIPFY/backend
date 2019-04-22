376b7de5eb80a23df5a75080e186635d427909b4
Jannis Froese <jf@vipfy.com>
2019-04-22 20:42:10 +0200

    update lots of stuff
    Should be safe. Npm audit only shows one vuln now
    (in some @google package I can't test right now).
    Some issues still remain, mostly with peer dependencies.
    I also skipped some update I felt less certain about.

--- src/services/sendinblue.js
+++ src/services/sendinblue.js
@@ -1,43 +1,43 @@
-import SibApiV3Sdk from "sib-api-v3-sdk";
-import { times, random } from "lodash";
+// import SibApiV3Sdk from "sib-api-v3-sdk";
+// import { times, random } from "lodash";
 
-const defaultClient = SibApiV3Sdk.ApiClient.instance;
+// const defaultClient = SibApiV3Sdk.ApiClient.instance;
 
-const apiKey = defaultClient.authentications["api-key"];
+// const apiKey = defaultClient.authentications["api-key"];
 
-apiKey.apiKey = process.env.SIB_KEY;
+// apiKey.apiKey = process.env.SIB_KEY;
 
-const password = times(20, () => random(35).toString(36)).join("");
+// const password = times(20, () => random(35).toString(36)).join("");
 
-const accountApi = new SibApiV3Sdk.AccountApi();
-const resellerApi = new SibApiV3Sdk.ResellerApi();
+// const accountApi = new SibApiV3Sdk.AccountApi();
+// const resellerApi = new SibApiV3Sdk.ResellerApi();
 
-export const getVipfyAccountData = async () => {
-  try {
-    const res = await accountApi.getAccount();
-    console.log(res);
-  } catch (err) {
-    console.log(err);
-  }
-};
+// export const getVipfyAccountData = async () => {
+//   try {
+//     const res = await accountApi.getAccount();
+//     console.log(res);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
-// createAccount Kauf den Plan und gib ihm Email credits
-export const createAccount = async (email, firstName, lastName, companyName) => {
-  try {
-    const options = {
-      resellerChild: new SibApiV3Sdk.CreateChild(email, firstName, lastName, companyName, password)
-    };
-    const res = await resellerApi.createResellerChild(options);
-    console.log("RES", res);
-    return res;
-  } catch (err) {
-    console.log("ERR", err.response);
-    return err.response.text;
-  }
-};
+// // createAccount Kauf den Plan und gib ihm Email credits
+// export const createAccount = async (email, firstName, lastName, companyName) => {
+//   try {
+//     const options = {
+//       resellerChild: new SibApiV3Sdk.CreateChild(email, firstName, lastName, companyName, password)
+//     };
+//     const res = await resellerApi.createResellerChild(options);
+//     console.log("RES", res);
+//     return res;
+//   } catch (err) {
+//     console.log("ERR", err.response);
+//     return err.response.text;
+//   }
+// };
 
-// changeEmailCredits bei changePlan des Users
+// // changeEmailCredits bei changePlan des Users
 
-// deleteAccount bei Kündigung des plans
+// // deleteAccount bei Kündigung des plans
 
-// addEmailCredits zum ersten des Monats
+// // addEmailCredits zum ersten des Monats
