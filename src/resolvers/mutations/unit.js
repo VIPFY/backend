018ae199039bc81a9fb55ebb2e3b4260cb323a96
gh05d <pc@vipfy.com>
2019-04-24 15:37:01 +0200

    Implemented updateEmployee and updateMyself

--- src/resolvers/mutations/unit.js
+++ src/resolvers/mutations/unit.js
@@ -2,7 +2,7 @@ import { decode } from "jsonwebtoken";
 import { requiresAuth, requiresRights } from "../../helpers/permissions";
 import { userPicFolder } from "../../constants";
 import { NormalError } from "../../errors";
-import { createLog } from "../../helpers/functions";
+import { createLog, companyCheck } from "../../helpers/functions";
 import { uploadUserImage } from "../../services/aws";
 /* eslint-disable no-unused-vars, prefer-destructuring */
 
@@ -106,5 +106,155 @@ export default {
           });
         }
       })
+  ),
+
+  updateMyself: requiresAuth.createResolver(
+    async (parent, { user }, { models, token, ip }) =>
+      models.sequelize.transaction(async ta => {
+        try {
+          const {
+            user: { unitid }
+          } = decode(token);
+
+          const { password, statisticdata, ...human } = user;
+          let updatedHuman;
+          if (password) {
+            throw new Error("You can't update the password this way!");
+          }
+
+          const oldHuman = await models.Human.findOne({
+            where: { unitid },
+            raw: true
+          });
+
+          if (statisticdata) {
+            updatedHuman = await models.Human.update(
+              {
+                statisticdata: { ...oldHuman.statisticdata, ...statisticdata },
+                ...human
+              },
+              { where: { unitid }, returning: true, transaction: ta }
+            );
+          } else {
+            updatedHuman = await models.Human.update(
+              { ...human },
+              { where: { unitid }, returning: true, transaction: ta }
+            );
+          }
+
+          await createLog(
+            ip,
+            "updateMyself",
+            { updateArgs: user, oldHuman, updatedHuman: updatedHuman[1] },
+            unitid,
+            ta
+          );
+
+          return { ...updatedHuman[1], ...human };
+        } catch (err) {
+          throw new NormalError({
+            message: err.message,
+            internalData: { err }
+          });
+        }
+      })
+  ),
+
+  updateEmployee: requiresRights(["edit-employee"]).createResolver(
+    async (parent, { user }, { models, token, ip }) => {
+      await models.sequelize.transaction(async ta => {
+        try {
+          const {
+            user: { unitid, company }
+          } = decode(token);
+
+          const { id, password, ...userData } = user;
+
+          if (password) {
+            throw new Error("You can't update the password this way!");
+          }
+
+          await companyCheck(company, unitid, id);
+
+          const toUpdate = {
+            Human: {},
+            Phone: [],
+            Email: [],
+            workPhones: []
+          };
+
+          Object.keys(userData).forEach(prop => {
+            if (prop.includes("phone")) {
+              toUpdate.Phone.push({ ...userData[prop] });
+            } else if (prop.includes("email")) {
+              toUpdate.Email.push({ ...userData[prop] });
+            } else if (prop.includes("workPhone")) {
+              toUpdate.workPhones.push({ ...userData[prop] });
+            } else if (prop == "address") {
+              toUpdate.Address = [{ ...userData[prop] }];
+            } else {
+              toUpdate.Human[prop] = userData[prop];
+            }
+          });
+
+          const promises = [];
+
+          if (toUpdate.hasOwnProperty("Human")) {
+            promises.push(
+              models.Human.update(
+                { ...toUpdate.Human },
+                { where: { unitid: id }, transaction: ta }
+              )
+            );
+
+            delete toUpdate.Human;
+          }
+
+          Object.keys(toUpdate).forEach(itemKey => {
+            toUpdate[itemKey].forEach(item => {
+              const model = itemKey == "workPhones" ? "Phone" : itemKey;
+
+              if (item.hasOwnProperty("id")) {
+                promises.push(
+                  models[model].update(
+                    { ...item },
+                    { where: { id: item.id }, transaction: ta }
+                  )
+                );
+              } else {
+                item.tags =
+                  itemKey == "Phone" || "Address" ? ["private"] : ["work"];
+
+                promises.push(
+                  models[model].create(
+                    { ...item, unitid: id },
+                    { transaction: ta }
+                  )
+                );
+              }
+            });
+          });
+
+          await Promise.all(promises);
+
+          await createLog(
+            ip,
+            "updateEmployee",
+            { updateArgs: user },
+            unitid,
+            ta
+          );
+
+          return true;
+        } catch (err) {
+          throw new NormalError({
+            message: err.message,
+            internalData: { err }
+          });
+        }
+      });
+
+      return models.User.findOne({ where: { id: user.id } });
+    }
   )
 };
