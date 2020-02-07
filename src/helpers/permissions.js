15b71e6407ef7253540018a44b9c8fa1fb29e2ae
gh05d <pc@vipfy.com>
2020-02-07 14:16:40 +0100

    Cleanup

--- src/helpers/permissions.js
+++ src/helpers/permissions.js
@@ -117,14 +117,15 @@ export const requiresAuth = createResolver(
   // all other cases handled by auth middleware
 );
 
-export const requiresDepartmentCheck = requiresAuth.createResolver(
-  async (_parent, args, { models, session }) => {
+export const requiresRights = rights =>
+  requiresAuth.createResolver(async (_parent, args, { models, session }) => {
     try {
-      if (args.departmentid) {
-        const {
-          user: { company }
-        } = decode(session.token);
+      const {
+        user: { unitid: holder, company }
+      } = await decode(session.token);
 
+      // Seems redundant @Jannis Froese
+      if (args.departmentid) {
         await checkCompanyMembership(
           models,
           company,
@@ -132,182 +133,150 @@ export const requiresDepartmentCheck = requiresAuth.createResolver(
           "department"
         );
       }
-    } catch (err) {
-      session.destroy(error => {
-        if (error) {
-          console.error(error);
+
+      if (args.licenceid) {
+        await checkLicenceMembership(models, company, args.licenceid);
+      }
+
+      if (args.orbitid) {
+        await checkOrbitMembership(models, company, args.orbitid);
+      }
+
+      if (args.teamid && args.teamid != "new") {
+        await checkCompanyMembership(models, company, args.teamid, "team");
+      }
+
+      if (args.userid && args.userid != "new") {
+        await checkCompanyMembership(models, company, args.userid, "user");
+
+        if (args.userid == holder) {
+          return;
         }
+      }
+
+      if (args.user && args.user.id && args.user.id != "new") {
+        await checkCompanyMembership(models, company, args.user.id, "user");
+
+        if (args.user.id == holder) {
+          return;
+        }
+      }
+
+      if (args.employeeid && args.employeeid != "new") {
+        await checkCompanyMembership(models, company, args.employeeid, "user");
+      }
+
+      if (args.unitid && args.unitid != "new") {
+        await checkCompanyMembership(models, company, args.unitid, "user");
+
+        if (args.unitid == holder) {
+          return;
+        }
+      }
+
+      if (args.userids) {
+        await Promise.all(
+          args.userids
+            .filter(id => id != "new")
+            .map(id => checkCompanyMembership(models, company, id, "user"))
+        );
+      }
+
+      if (args.unitids) {
+        await Promise.all(
+          args.unitids
+            .filter(id => id != "new")
+            .map(id => checkCompanyMembership(models, company, id, "user"))
+        );
+      }
+
+      const rightsCheck = {};
+      let index = 0;
+
+      rights.forEach((_r, i) => {
+        rightsCheck[i] = true;
       });
-      throw new AuthError(err);
-    }
-  }
-);
 
-export const requiresRights = rights =>
-  requiresDepartmentCheck.createResolver(
-    async (_parent, args, { models, session }) => {
-      try {
-        const {
-          user: { unitid: holder, company }
-        } = await decode(session.token);
-
-        if (args.departmentid) {
-          await checkCompanyMembership(
-            models,
-            company,
-            args.departmentid,
-            "department"
-          );
-        }
-
-        if (args.licenceid) {
-          await checkLicenceMembership(models, company, args.licenceid);
-        }
-
-        if (args.orbitid) {
-          await checkOrbitMembership(models, company, args.orbitid);
-        }
-
-        if (args.teamid && args.teamid != "new") {
-          await checkCompanyMembership(models, company, args.teamid, "team");
-        }
-
-        if (args.userid && args.userid != "new") {
-          await checkCompanyMembership(models, company, args.userid, "user");
-
-          if (args.userid == holder) {
-            return;
+      for await (const right of rights) {
+        if (typeof right == "string") {
+          if (
+            right == "myself" &&
+            ((args.userid && args.userid == holder) ||
+              (args.employeeid && args.employeeid == holder) ||
+              (args.user && args.user.id && args.user.id == holder) ||
+              (!args.user && !args.userid && !args.employeeid))
+          ) {
+            break;
           }
-        }
 
-        if (args.user && args.user.id && args.user.id != "new") {
-          await checkCompanyMembership(models, company, args.user.id, "user");
-
-          if (args.user.id == holder) {
-            return;
-          }
-        }
-
-        if (args.employeeid && args.employeeid != "new") {
-          await checkCompanyMembership(
-            models,
-            company,
-            args.employeeid,
-            "user"
-          );
-        }
-
-        if (args.unitid && args.unitid != "new") {
-          await checkCompanyMembership(models, company, args.unitid, "user");
-
-          if (args.unitid == holder) {
-            return;
-          }
-        }
-
-        if (args.userids) {
-          await Promise.all(
-            args.userids
-              .filter(id => id != "new")
-              .map(id => checkCompanyMembership(models, company, id, "user"))
-          );
-        }
-
-        if (args.unitids) {
-          await Promise.all(
-            args.unitids
-              .filter(id => id != "new")
-              .map(id => checkCompanyMembership(models, company, id, "user"))
-          );
-        }
-
-        const rightsCheck = {};
-        let index = 0;
-
-        rights.forEach((_r, i) => {
-          rightsCheck[i] = true;
-        });
-
-        for await (const right of rights) {
-          if (typeof right == "string") {
-            if (
-              right == "myself" &&
-              ((args.userid && args.userid == holder) ||
-                (args.employeeid && args.employeeid == holder) ||
-                (args.user && args.user.id && args.user.id == holder) ||
-                (!args.user && !args.userid && !args.employeeid))
-            ) {
+          if (right == "myself" && args.assignmentid) {
+            const r = await models.sequelize.LicenceRight.findOne({
+              where: {
+                unitid: holder,
+                id: args.assignmentid,
+                endtime: { [models.Op.lt]: models.sequelize.fn("NOW") }
+              }
+            });
+            if (r) {
               break;
             }
+          }
 
-            if (right == "myself" && args.assignmentid) {
-              const r = await models.sequelize.LicenceRight.findOne({
-                where: {
-                  unitid: holder,
-                  id: args.assignmentid,
-                  endtime: { [models.Op.lt]: models.sequelize.fn("NOW") }
-                }
-              });
-              if (r) {
-                break;
-              }
-            }
+          const hasRight = await models.Right.findOne({
+            where: models.sequelize.and(
+              { holder },
+              { forunit: { [models.Op.or]: [company, null] } },
+              models.sequelize.or({ type: right }, { type: "admin" })
+            )
+          });
 
+          if (hasRight) {
+            break;
+          } else {
+            rightsCheck[index] = false;
+          }
+        } else {
+          for await (const subRight of right) {
             const hasRight = await models.Right.findOne({
               where: models.sequelize.and(
                 { holder },
                 { forunit: { [models.Op.or]: [company, null] } },
-                models.sequelize.or({ type: right }, { type: "admin" })
+                models.sequelize.or({ type: subRight }, { type: "admin" })
               )
             });
 
-            if (hasRight) {
-              break;
-            } else {
+            if (!hasRight) {
               rightsCheck[index] = false;
-            }
-          } else {
-            for await (const subRight of right) {
-              const hasRight = await models.Right.findOne({
-                where: models.sequelize.and(
-                  { holder },
-                  { forunit: { [models.Op.or]: [company, null] } },
-                  models.sequelize.or({ type: subRight }, { type: "admin" })
-                )
-              });
-
-              if (!hasRight) {
-                rightsCheck[index] = false;
-                break;
-              }
+              break;
             }
           }
-          index++;
         }
+        index++;
+      }
 
-        const okay = Object.values(rightsCheck).some(val => val === true);
-        if (!okay) {
-          throw new RightsError();
-        }
-      } catch (err) {
-        if (err instanceof RightsError) {
-          console.error(err);
-          throw err;
-        } else {
-          session.destroy(error => {
-            if (error) {
-              console.error(error);
-            }
-          });
-          console.error(err);
-          throw new AuthError({
-            message:
-              "Opps, something went wrong. Please report this error with id auth_1"
-          });
-        }
+      const okay = Object.values(rightsCheck).some(val => val === true);
+      if (!okay) {
+        throw new RightsError();
+      }
+      console.log("RIGHTS_CHECK OKAY");
+    } catch (err) {
+      if (err instanceof RightsError) {
+        console.error(err);
+        throw err;
+      } else {
+        console.error(err);
+        session.destroy(error => {
+          if (error) {
+            console.error(error);
+          }
+        });
+        throw new AuthError({
+          message:
+            "Opps, something went wrong. Please report this error with id auth_1"
+        });
       }
     }
-  );
+  });
 
 export const requiresMessageGroupRights = rights =>
   requiresAuth.createResolver(async (_p, args, { models, session }) => {
