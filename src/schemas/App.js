47516519cb686fe0404c851dc1f3d0b3b0532cb7
gh05d <pc@vipfy.com>
2020-09-12 16:37:47 +0200

    Updated schema for alternatives

--- src/schemas/App.js
+++ src/schemas/App.js
@@ -22,7 +22,7 @@ const appFields = `
   ratings: Ratings
   externalstatistics: ExternalStatistics
   tags: [String]
-  alternatives: JSON
+  alternatives: [AppAlternative]
 `;
 
 const basicLicenceFields = `
@@ -46,6 +46,13 @@ export const types = `
   type ExecuteApp {
     ${appFields}
     internaldata: JSON
+  }
+
+  type AppAlternative {
+    app: AppDetails!
+    name: String!
+    rating: Float!
+    reviews: Int!
   }
 
   type Ratings {
