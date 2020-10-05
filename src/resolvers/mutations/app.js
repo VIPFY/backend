4198e8b6b816a676c94907144742b91618e7bb3a
gh05d <pc@vipfy.com>
2020-10-05 11:58:09 +0200

    Updated function with optional parameter

--- src/resolvers/mutations/app.js
+++ src/resolvers/mutations/app.js
@@ -211,14 +211,16 @@ export default {
     }
   ),
 
-  sendDownloadLink: async (_p, { email }) => {
+  sendDownloadLink: async (_p, { email, isMac }) => {
     try {
       await sendEmail({
-        templateId: "d-dbe6fd61bc654b81b036cfced48f2066",
+        templateId: isMac
+          ? "d-e31dc52fbac54edba50f3e6586a714bb"
+          : "d-dbe6fd61bc654b81b036cfced48f2066",
         fromName: "VIPFY",
         personalizations: [{ to: [{ email }], dynamic_template_data: {} }],
       });
-      console.log("Serverlogs", "Sent download link to " + email);
+      console.log("Serverlogs", `Sent download link to ${email}`);
 
       return true;
     } catch (err) {
